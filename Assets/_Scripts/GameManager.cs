a674c6359641918a4e20c6a3caa32d63469aa582
Shuliingk <117281266+ShulingK@users.noreply.github.com>
2026-03-21 11:13:14 +0100

    [UPT]

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,19 +1,78 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    public void Awake()
+    {
+        if (_placementEventChannel != null)
+            SubscribePlacementEventChannel();
+    }
 
     #region Rounds
     Round _currentRound;
     public void SetActiveRound(Round round) => _currentRound = round;
     public Round GetActiveRound() => _currentRound;
 
-    void VerifySuccessRound()
+    void CheckSuccessRound()
     {
-        // logique
+        List<SRound> _tempRound = new List<SRound>();
+        _tempRound = GetActiveRound().GetRounds();
+
+        foreach(EmotionPlacement emotions in _emotionAlreadyPlaced)
+        {
+            if (emotion)
+        }
     }
 
     #endregion
 
+    #region Placement Event
+    [SerializeField] PlacementEventChannel _placementEventChannel;
+    
+    private struct EmotionPlacement
+    {
+        public Emotion emotion;
+        public PlayerSlot slot;
+    }
+
+    List<EmotionPlacement> _emotionAlreadyPlaced = new List<EmotionPlacement>();
+
+    public void SubscribePlacementEventChannel()
+    {
+        _placementEventChannel.OnEmotionPlaced += OnEmotionPlaced;
+    }
+
+    public void UnsubscribePlacementEventChannel()
+    {
+        _placementEventChannel.OnEmotionPlaced -= OnEmotionPlaced;
+    }
+
+    private void OnEmotionPlaced(Emotion emotion, PlayerSlot slot, bool isDropped)
+    {
+        EmotionPlacement item;
+        item.emotion = emotion;
+        item.slot = slot;
+
+        if (isDropped)
+        {
+            // verifier s'il y en a pas déjà 1 ? 
+            _emotionAlreadyPlaced.Add(item);
+        }
+        else
+        {
+            for(int i = 0; i < _emotionAlreadyPlaced.Count; i++)
+            {
+                if (_emotionAlreadyPlaced[i].slot == slot && _emotionAlreadyPlaced[i].emotion == emotion)
+                {
+                    _emotionAlreadyPlaced.Remove(_emotionAlreadyPlaced[i]);
+                    break;
+                }
+            }
+        }
+
+        CheckSuccessRound();
+    }
+    #endregion
 }
--- Assets/_Scripts/ScriptableObject/PlacementEventChannel.cs
+++ Assets/_Scripts/ScriptableObject/PlacementEventChannel.cs
@@ -4,10 +4,10 @@ using System;
 [CreateAssetMenu(menuName = "Events/Placement Event")]
 public class PlacementEventChannel : ScriptableObject
 {
-    public Action<Emotion, PlayerSlot> OnEmotionPlaced;
+    public Action<Emotion, PlayerSlot, bool > OnEmotionPlaced;
 
-    public void RaiseEvent(Emotion emotionID, PlayerSlot playerSlotID)
+    public void RaiseEvent(Emotion emotionID, PlayerSlot playerSlotID, bool isDropped)
     {
-        OnEmotionPlaced?.Invoke(emotionID, playerSlotID);
+        OnEmotionPlaced?.Invoke(emotionID, playerSlotID, isDropped);
     }
 }
